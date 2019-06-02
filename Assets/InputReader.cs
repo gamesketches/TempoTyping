8956b799caaf0eaa44aa83b4fbf75c5283d89cd0
gamesketches <sam.von.ehren@gmail.com>
2019-06-02 17:22:32 -0400

    got basics working, sound and text can be swapped out, starting on animations

--- Assets/InputReader.cs
+++ Assets/InputReader.cs
@@ -11,23 +11,36 @@ public class InputReader : MonoBehaviour {
 	Text bpmDisplay;
 	int cursor;
 	float curTime;
-	float BPM;
+	public float WPM;
+	public float BPM;
 	public int marginSize;
 	public int leadLetters;
+	public int leadingWhiteSpace;
 	public float targetBPM;
 	public float shiftAmount;
 	public float smoothTime;
 	float currentVelocity;
+	Queue<float> charsEntered;
 	AudioSource audioSource;
 	AudioMixer mixer;
+	int scratchCursor;
 	// Use this for initialization
 	void Awake () {
+		charsEntered = new Queue<float>();
+		scratchCursor = -1;
 		textDisplay = GetComponent<Text>();
 		bpmDisplay = GameObject.Find("BPMDisplay").GetComponent<Text>();
 		BPM = targetBPM;
 		audioSource = GetComponent<AudioSource>();
 		mixer = Resources.Load<AudioMixer>("AudioMixer"); 
+		TextAsset textFile = Resources.Load<TextAsset>("text");
+		targetText = textFile.text.Replace("\n", " ");
 		curTime = 0;
+		for(int i = 0; i < leadingWhiteSpace; i++) {
+			targetText = string.Concat(" ", targetText);
+		}
+		leadLetters += leadingWhiteSpace;
+		cursor += leadingWhiteSpace;
 		audioSource.Play();
 		
 	}
@@ -37,10 +50,32 @@ public class InputReader : MonoBehaviour {
 		foreach(char c in Input.inputString) {
 			if(targetText[cursor] == c) {
 				cursor++;
+				charsEntered.Enqueue(curTime);
+				scratchCursor = -1;
+			}
+			else if(scratchCursor > 0) {
+				if(targetText[scratchCursor] == c) {
+					audioSource.time += WPM / 60;
+				}
+				else if(targetText[scratchCursor - 1] == c) {
+					audioSource.time -= WPM / 60;
+				}
+			}
+			/*else if(cursor > 0 && targetText[cursor - 1] == c) {
+				audioSource.time -= WPM / 60;
+				curTime -= Time.deltaTime;
+				scratchCursor = cursor;
+				cursor++;
+				Debug.Log(WPM / 60 / 60);
+			}*/
+		}
+		if(charsEntered.Count > 0){
+			while(curTime - charsEntered.Peek() > 60) {
+				charsEntered.Dequeue();
 			}
 		}
 		if(cursor > leadLetters) {
-			float WPM = (cursor / 5f) / (curTime / 60);
+			WPM = (charsEntered.Count / 5f) / (curTime / 60);//(cursor / 5f) / (curTime / 60);
 			if(Mathf.Abs(BPM - WPM) > marginSize) {
 				if(BPM > WPM) {
 					BPM -= marginSize;
@@ -53,8 +88,12 @@ public class InputReader : MonoBehaviour {
 			audioSource.pitch = Mathf.SmoothDamp(audioSource.pitch, BPM / targetBPM, ref currentVelocity, smoothTime);
 			mixer.SetFloat("pitchBend", 1f / audioSource.pitch);
 		}
-		textDisplay.text = targetText.Substring(cursor);
-		curTime += Time.deltaTime;
+		if(scratchCursor < 0) {
+			curTime += Time.deltaTime;
+			textDisplay.text = "<color=#ff0000>" + targetText.Substring(cursor - leadingWhiteSpace, leadingWhiteSpace) + "</color>" + targetText.Substring(cursor);
+		} else {
+			textDisplay.text = "<color=#ff000>" + targetText.Substring(scratchCursor - 1 - leadingWhiteSpace, leadingWhiteSpace) + "</color>" + targetText.Substring(cursor);
+		}
 		//Debug.Log(BPM);
 	}
 }
---
+++ Assets/SpeedometerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedometerControl : MonoBehaviour {
+
+	Image speedometerImage;
+	InputReader inputReader;
+
+	// Use this for initialization
+	void Start () {
+		speedometerImage = GetComponent<Image>();	
+		inputReader = GameObject.Find("TextDisplay").GetComponent<InputReader>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float diff = inputReader.targetBPM - inputReader.WPM;
+		transform.rotation = Quaternion.Euler(0, 0, diff);
+	}
+}
